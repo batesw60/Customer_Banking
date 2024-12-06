c34b9768651368b8a072e514e441f082c2c54c0d
wfrescas <128990642+batesw60@users.noreply.github.com>
2024-12-05 20:35:45 -0700

    Update Input_Type_Check.py
    Update function description.

--- Input_Type_Check.py
+++ Input_Type_Check.py
@@ -27,7 +27,16 @@ def is_input_the_type(input, input_type):
             return False, None
 
 def check_input(input_from_user, input_type, question):
+    """
+    Create a function called check_input that checks if the input is the correct type and if so converts the string to that type.
+    Args:
+        input_from_user (string): User value input
+        input_type (float or int): the type input you are checkig for
+        question (string): The question you asked the user to get the input_from_user
 
+    Returns:
+        the float or int of the users string input.
+    """
     converted_string = ""
     correct_format = False
 
