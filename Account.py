ff089ea3e09489cffb7d71833e70462f7449f695
wfrescas <128990642+batesw60@users.noreply.github.com>
2024-12-05 20:26:44 -0700

    Update Account.py
    Final Changes

--- Account.py
+++ Account.py
@@ -1,16 +1,16 @@
-""" Create an Account class with methods"""
+""" Create a Account class with methods"""
 
 class Account:
     """Creating an Account class with methods"""
-    def __init__(self, balance, interest_gained):
+    def __init__(self, balance, interest):
         self.balance = balance
-        self.interest_gained = interest_gained
+        self.interest = interest
 
     # This method gets the balance on the account.
     def get_balance(self):
         """Outputs the balance of the account."""
         return self.balance
-    
+
     # This method sets the balance of the account.
     def set_balance(self, balance):
         """Sets the balance for the for the account"""
@@ -24,4 +24,4 @@ class Account:
     # The method sets the interest gained for the account.
     def set_interest_gained(self, interest):
         """Sets the interest gained for the the account"""
-        self.interest_gained = interest
+        self.interest = interest
