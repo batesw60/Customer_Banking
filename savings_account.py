f32f6059b971cbd67601bd9e9475dac2f5d95a24
wfrescas <128990642+batesw60@users.noreply.github.com>
2024-12-05 20:29:11 -0700

    Update savings_account.py
    Final Changes

--- savings_account.py
+++ savings_account.py
@@ -19,7 +19,7 @@ def create_savings_account(balance, interest_rate, months):
     my_savings_account = Account(balance, 0)
 
     # Calculate interest earned
-    interest_earned = balance * interest_rate * months/12
+    interest_earned = balance * interest_rate * months / 12
 
     # Update the savings account balance by adding the interest earned
     balance += interest_earned
